d1b73d9e3444afeabd7bd055921146332ccbe9cf
ana-schoffen <ra115465@uem.br>
2021-06-03 14:51:22 -0300

    Versão utilizando final form

--- containers/ItemForms.js
+++ containers/ItemForms.js
@@ -1,6 +1,91 @@
 import React, { useState } from "react";
 import api from "../services/api";
 import { TextField, Button, Grid, FormControl, FormControlLabel, Switch } from "@material-ui/core";
+import { Form, Field } from "react-final-form";
+
+function TextFieldA(props) {
+  const errorMessage = props.meta.error;
+  const shoudShowError = !!errorMessage && props.meta.touched;
+
+  const value = props.input.value;
+  const onChange = (ev) => props.input.onChange(ev.target.value);
+  return (
+    <>
+      {props.label}
+      <TextField
+        fullWidth
+        label={value}
+        onChange={onChange} {...props.input}
+      />
+      {shoudShowError && errorMessage}
+    </>
+  );
+}
+
+function ItemForm() {
+  const [ativo, setAtivo] = useState(true);
+
+  const onSubmit = (formValues) => {
+    return api
+      .createItem({
+        codigo: formValues.codigo,
+        descricao: formValues.descricao,
+        ativo: ativo,
+      })
+      .then(() => alert("Salvo com sucesso!"))
+      .catch((err) => alert(`Erro: ${err.message}`));
+  };
+
+  const validate = (formValues) => {
+    const error = {};
+    if (!formValues.codigo) error.codigo = "Código é um campo obrigatório";
+    if (!formValues.descricao)
+      error.descricao = "Descrição é um campo obrigatório";
+
+    return error;
+  };
+
+  const initialValues = { codigo: "000", descricao: "descrição" };
+
+  return (
+    <div>
+      <Form
+        onSubmit={onSubmit}
+        validate={validate}
+        initialValues={initialValues}
+      >
+        {(formProps) => (
+          <form onSubmit={formProps.handleSubmit}>
+            <Field name="codigo" label="Código" component={TextFieldA} />
+            <br />
+
+            <Field name="descricao" label="Descrição" component={TextFieldA} />
+            <br />
+
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={ativo}
+                  onChange={(ev) => setAtivo(ev.target.checked)}
+                />
+              }
+              label="Ativo"
+            />
+            <br />
+
+            <button type="submit">Salvar</button>
+          </form>
+        )}
+      </Form>
+    </div>
+  );
+}
+
+/* implementação sem o final form
+import React, { useState } from "react";
+import api from "../services/api";
+import { TextField, Button, Grid, FormControl, FormControlLabel, Switch } from "@material-ui/core";
+import { Form, Field } from "react-final-form";
 
 function ItemForm() {
   const [codigo, setCodigo] = useState("");
@@ -59,5 +144,5 @@ function ItemForm() {
     </div >
   );
 }
-
+*/
 export default ItemForm;
--- containers/configurations.js
+++ containers/configurations.js
@@ -8,63 +8,72 @@ import {
   FormControl,
   Typography,
 } from "@material-ui/core";
+import { Form, Field } from "react-final-form";
 
 function Configuration() {
   const [usuario, setUsuario] = useState("");
   const [email, setEmail] = useState("");
   const [receberNotificacoes, setReceberNotificacoes] = useState(false);
 
-  return (
-    <div>
+  function TextFieldA(props) {
+    const errorMessage = props.meta.error;
+    const shouldShowError = !!errorMessage && props.meta.touched;
+
+    const value = props.input.value;
+    const onChange = ev => props.input.onChange(ev.target.value);
+    return <>
+      {props.label}:
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <TextField
             fullWidth
-            label="Usuário"
-            value={usuario}
-            onChange={(ev) => setUsuario(ev.target.value)}
+            label={value}
+            onChange={onChange} {...props.input}
           />
-        </Grid>
-        <Grid item xs>
-          <TextField
-            fullWidth
-            label="E-mail"
-            value={email}
-            onChange={(ev) => setEmail(ev.target.value)}
-          />
-        </Grid>
-        <Grid item>
-          <FormControlLabel
-            fullWidth
-            control={
-              <Switch
-                checked={receberNotificacoes}
-                onChange={(ev) => setReceberNotificacoes(ev.target.checked)}
-              />
-            }
-            label="Receber notificações"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl>
-            <Button variant="contined" color="primary">
-              Salvar
-            </Button>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="subtitle1">Usuário: {usuario}</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="subtitle1">
-            Email: {email} (
-            {receberNotificacoes
-              ? "Receber notificações"
-              : "Não receber notificações"}
-            )
-          </Typography>
         </Grid>
       </Grid>
+
+      {shouldShowError && errorMessage}
+
+
+    </>
+
+  }
+  const onSubmit = formValues => alert(
+    `Nome: ${formValues.nome}\nE-mail: ${formValues.email}`
+  );
+  return (
+    <div>
+      <Form
+        onSubmit={onSubmit}
+      >
+        {(formProps) => (
+          <form onSubmit={formProps.handleSubmit}>
+            <Field name="nome" label="Nome" component={TextFieldA} />
+            <br />
+
+            <Field name="email" label="E-mail" component={TextFieldA} />
+            <br />
+
+            <Grid item>
+              <FormControlLabel
+                fullWidth
+                control={
+                  <Switch
+                    checked={receberNotificacoes}
+                    onChange={(ev) => setReceberNotificacoes(ev.target.checked)}
+                  />
+                }
+                label="Receber notificações"
+              />
+            </Grid>
+
+            <br />
+
+            <button type="submit">Salvar</button>
+          </form>
+        )}
+      </Form>
     </div>
   );
 }
